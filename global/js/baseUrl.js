8b210aa63326afcbe61bf21f92be25a1b5e816f1
mikublog <786601672@qq.com>
2020-11-23 13:07:08 +0800

    更新代码

--- global/js/baseUrl.js
+++ global/js/baseUrl.js
@@ -1,5 +1,5 @@
-export const baseUrl = "http://192.168.0.105:8085"
+export const baseUrl = "http://192.168.43.171:8080"
 // appid
-export const appid = "wx7f4e077912aa74ce"
+export const appid = "wxe64670b15ac1a357"
 // h5回调地址
 export const callbackUrl = "https://basefront.whalenect.com/h5"
